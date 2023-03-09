414e5b54b034f035d8431085dd7031d45aa39b8c
Roman Alcantara <romanariel.alcantara@gmail.com>
2023-03-08 18:20:52 -0800

    Add project files.

---
+++ 2doParcial/Models/Usuario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _2doParcial.Models;
+
+public partial class Usuario
+{
+    public int Id { get; set; }
+
+    public string? Nombre { get; set; }
+
+    public string? Apellido { get; set; }
+
+    public string? Carrera { get; set; }
+}
